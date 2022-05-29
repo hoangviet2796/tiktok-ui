9fa3812c47f0bd88ca14ab7a47a40803348b6c7a
Viet <nguyenkieuhoangviet@gmail.com>
2022-05-30 00:31:00 +0700

    update

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+//Layout
+import HeaderOnly from '../Layout/HeaderOnly';
+
+//Pages
+import Home from '../pages/Home';
+import Following from '../pages/Following';
+import Upload from '../pages/Upload';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
